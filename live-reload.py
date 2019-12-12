3d8d1a468e1282a32b3a1900b0d102128e879ace
Nick Smith <nmsmith65@gmail.com>
2019-12-12 20:36:17 +1100

    Separate Pug/HTML, JS, and CSS

--- live-reload.py
+++ live-reload.py
@@ -5,4 +5,6 @@ server = Server()
 
 # Run Pug to rebuild the code when the file is changed
 server.watch('src/index.pug', 'pug src --out site')
+server.watch('src/app.js', 'cp src/app.js site/app.js')
+server.watch('src/style.css', 'cp src/style.css site/style.css')
 server.serve(root='site')
---
+++ src/app.js
@@ -0,0 +1,64 @@
+'use strict';
+/* global Vue */
+
+Vue.component('thing', {
+    props: ['x'],
+    template: '<li>The current count is {{ x }}</li>'
+});
+
+let restoredState;
+
+// Vue makes a copy of this data, so keeping it out of scope
+// will prevent us from accidentally linking back to it!
+{
+    let products = [{x: 3}, {y: 9}, {z: 0}, {w: 0}];
+    
+    let initialState = {
+        item: 'Thing',
+        products: products,
+        count: 0,
+        cursor: products[0],
+        toJSON () {
+            return this
+        },
+    }
+    
+    restoredState = (localStorage.state && localStorage.state != "undefined")
+        ? {...initialState, ...JSON.retrocycle(JSON.parse(localStorage.state))}
+        : initialState
+}
+
+new Vue({
+    el: '#app',
+    // "Essential" state values.
+    data: restoredState,
+    // Derived state values. These stay cached once computed.
+    // These are read-only by default, but you can add setters as well.
+    computed: {
+        totalProducts () {
+            return this.products.reduce((curr, x) => curr + x);
+        }
+    },
+    // A method that runs on app start. Can be used to perform external effects.
+    created () {
+        // Set up save-on-close behaviour
+        window.addEventListener('beforeunload', this.saveState)
+        this.cursor = this.products[0]
+    },
+    // Methods should (only) be used to perform a state transition or an external effect.
+    // They are useful for polling external state. Return values are not cached.
+    methods: {
+        increment () {
+            this.cursor.x += 1
+        },
+        saveState () {
+            // TODO: Have data auto-save periodically (every 10 sec?)
+            localStorage.state = JSON.stringify(JSON.decycle(this.$data))
+        },
+    },
+    // For running external effects when a "data" or "computed" property changes.
+    // Use this over a computed property if you need to perform asynchronous work (e.g. an API call).
+    // This is an imperative event-response model.
+    watch: {
+    },
+});
