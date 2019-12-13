9a32afb9111e961ef97e5557d1932a0686cbcfa2
Nick Smith <nmsmith65@gmail.com>
2019-12-13 13:49:24 +1100

    ESLint is being weird on Pug files. Hide Pug.

--- live-reload.py
+++ live-reload.py
@@ -4,7 +4,7 @@ from livereload import Server, shell
 server = Server()
 
 # Run Pug to rebuild the code when the file is changed
-server.watch('src/index.pug', 'npx pug src --out site')
+server.watch('src/index.pug', 'npx pug --pretty src --out site')
 server.watch('src/app.js', 'cp src/app.js site/app.js')
 server.watch('src/style.scss', 'npx sass src/style.scss site/style.css')
 server.serve(root='site')
