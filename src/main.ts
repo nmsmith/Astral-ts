edbb3b6dcf46d4ef5557d602af0bfc09edfd14a4
Nick Smith <nmsmith65@gmail.com>
2020-02-22 13:16:09 +1100

    Enter rules as plaintext, for my own sanity

--- src/main.ts
+++ src/main.ts
@@ -2,156 +2,48 @@ import "./reset.css"
 import "./styles/styles.scss"
 import "./globals"
 import Cycle from "json-cycle"
+import { toRefs } from "@vue/reactivity"
 import { WithDerivedProps, DerivedProps, withDerivedProps } from "./libs/lib-derived-state"
 import * as Registry from "./concept-registry"
-import { $if, $for, app, div, p, br, button, input, span } from "./libs/lib-view"
+import { $if, $for, app, div, p, br, button, input, textarea, span } from "./libs/lib-view"
 import { textBox, TextBoxState } from "./views/text-box"
-import { searchBox, SearchBoxState } from "./views/search-box"
-import { SearchResult } from "./libs/fuzzy-prefix-dict"
 
 //#region  --- Essential & derived state ---
-
-type Concept = Registry.Concept
-
-type SearchState = SearchBoxState<Concept> & {
-    registries: Registry.T[]
-}
-
-function SearchState<Location, Result>(registries: Registry.T[]): SearchState {
-    return {
-        selection: {isOccurring: false},
-        text: "",
-        resultSelected: null,
-        // add results & resultSelected as derived state
-        registries,
-    } as SearchState
-}
-
-function searchResults(search: SearchState): Registry.SearchResult[] {
-    let searchText: string
-    let errorTolerance: number
-    if (search.selection.isOccurring && search.selection.textChanged === false) {
-        searchText = ""
-        errorTolerance = 0
-    }
-    else {
-        searchText = search.text
-        errorTolerance = (search.text.length <= 1) ? 0 : 1
-    }
-    // Search all given registries
-    const searchResults: Registry.SearchResult[] = []
-    search.registries.forEach(r =>
-        searchResults.push(
-            ...Registry.findConceptsWithLabelPrefix(r, searchText, errorTolerance)
-        )
-    )
-    // Sort the results by closeness
-    if (searchText.length > 1) {
-        searchResults.sort((a, b) => a.distance - b.distance)
-    }
-    // Put the exact match at the top, if any.
-    // TODO: If we later allow multiple concepts to have the same label, then
-    // this ordering can lead to a hazardous outcome: the wrong concept gets
-    // put at the top of the search results, and autocomplete switches it out.
-    for (let i = 0; i < searchResults.length; ++i) {
-        if (searchResults[i].key === search.text) {
-            searchResults.unshift(searchResults.removeAt(i))
-            break
-        }
-    }
-    return searchResults
-}
-
 interface Fact {
-    readonly relation: SearchState
-    readonly objects: SearchState[]
-}
-
-function Fact(relationRegistry: Registry.T): Fact {
-    return {
-        relation: SearchState([relationRegistry]),
-        objects: [],
-    }
-}
-
-const factDerivedProps: DerivedProps<Fact> = {
-    relation:  { results: searchResults },
-    objects: { results: searchResults },
+    readonly relation: string
+    readonly objects: string[]
 }
 
 interface Rule {
-    readonly ruleConcept: Concept // rules are themselves concepts to make them searchable
-    readonly labelBoxState: TextBoxState
-    readonly varRegistry: Registry.T // for local variables
-    readonly head: Fact
-    readonly body: Fact[]
+    readonly label: string,
+    readonly rawText: string
+    readonly lastParsed: null | {
+        readonly rawText: string
+        readonly head: Fact
+        readonly body: Fact[]
+    }
 }
 
-function Rule(ruleRegistry: Registry.T, relationRegistry: Registry.T): Rule {
-    const ruleConcept = Registry.newConcept(ruleRegistry)
-    const varRegistry = Registry.empty("var")
-
+function Rule(): Rule {
     return {
-        ruleConcept,
-        labelBoxState: {focused: false, text: ruleConcept.label} as TextBoxState,
-        varRegistry,
-        head: Fact(relationRegistry),
-        body: [],
-    } as Rule
-}
-
-interface RuleView {
-    readonly rules: Rule[]
+        label: "",
+        rawText: "",
+        lastParsed: null,
+    }
 }
 
 interface State {
-    readonly relationRegistry: Registry.T
-    readonly objectRegistry: Registry.T // the database Domain
-    readonly ruleRegistry: Registry.T // for naming and searching for specific rules
     readonly rules: Rule[]
-    currentView: RuleView
-// derived state
-    readonly allRelations: Registry.SearchResult[]
-    readonly allObjects: Registry.SearchResult[]
 }
 
 function createState(existingState?: State): WithDerivedProps<State> {
-    const relationRegistry = Registry.empty("relation")
-    const objectRegistry = Registry.empty("object")
     const essentialState = existingState !== undefined ? existingState : {
-        relationRegistry,
-        objectRegistry,
-        ruleRegistry:
-            Registry.empty("rule"),
         rules:
             [] as Rule[],
-        currentView:
-            {rules: []},
-        // TODO: Shouldn't require these in the essential state
-        allRelations: [],
-        allObjects: [],
     }
     return withDerivedProps<State>(essentialState, {
         /* eslint-disable @typescript-eslint/explicit-function-return-type */
-        rules: {
-            labelBoxState: {
-                textIsValid: state => state.text.length > 0,
-            },
-            head: factDerivedProps,
-            body: factDerivedProps,
-        },
-        allRelations: state => searchResults({
-            selection: {isOccurring: false},
-            text: "",
-            resultSelected: null,
-            registries: [state.relationRegistry],
-        } as SearchState),
-        allObjects: state => searchResults({
-            selection: {isOccurring: false},
-            text: "",
-            resultSelected: null,
-            registries: [state.objectRegistry],
-        } as SearchState),
+        
     })
 }
 
@@ -215,77 +107,8 @@ function resetState(): void {
 //#region  --- The view & transition logic ----
 
 function newRule(i: number): void {
-    state.rules.insert(i, Rule(state.ruleRegistry, state.relationRegistry))
+    state.rules.insert(i, Rule())
 }
-
-function newPremise(rule: Rule): void {
-    rule.body.push(Fact(state.relationRegistry))
-}
-
-const relationRefEl = (item: SearchState, className: string): HTMLElement =>
-    searchBox (item, {
-        borderAlwaysVisible: false,
-        inputTextStyle: className,
-        unmatchingInputTextStyle: "labelForNothing",
-        showNothingOption: {
-            text: "nothing",
-            textStyle: "labelForNothingOption",
-        },
-    })
-
-const objectRefEl = (allRefs: SearchState[], thisRef: SearchState, varRegistry: Registry.T, className: string): HTMLElement =>
-    searchBox (thisRef, {
-        borderAlwaysVisible: false,
-        inputTextStyle: className,
-        unmatchingInputTextStyle: "labelForNothing",
-        showNothingOption: {
-            text: "nothing",
-            textStyle: "labelForNothingOption",
-        },
-        onKeyDown(key: string, input: HTMLInputElement) {
-            if ((key === "Backspace" || key === "Delete") && thisRef.text === "") {
-                // Delete the search box
-                allRefs.removeAt(allRefs.indexOf(thisRef))
-                return true
-            }
-            else if (key === ",") {
-                // Split the box/text into two search boxes
-                const newSearchBox = SearchState([state.objectRegistry, varRegistry])
-                if (input.selectionStart !== null) {
-                    const beforeComma = thisRef.text.slice(0, input.selectionStart)
-                    const afterComma = thisRef.text.slice(input.selectionStart)
-                    thisRef.text = beforeComma
-                    newSearchBox.text = afterComma
-                }
-                
-                allRefs.insert(allRefs.indexOf(thisRef) + 1, newSearchBox)
-                // Proactively blur the box, since it will otherwise be blurred in the middle of the view update (as its siblings are shuffled around).
-                // This ensures we update its rendering.
-                return true
-            }
-            else return false
-        },
-    })
-
-const factEl = (fact: Fact, varRegistry: Registry.T): HTMLElement =>
-    div ({class: "fact"}, [
-        div ({class: "row"}, [
-            relationRefEl (fact.relation, "labelForRelation"),
-            span ("(", {class: "factText"}),
-            $for (() => fact.objects.slice(0, -1), obj => [
-                objectRefEl (fact.objects, obj, varRegistry, "labelForObject"),
-                span (",", {class: "factComma"}),
-            ]),
-            $for (() => fact.objects.slice(-1), obj => [
-                objectRefEl (fact.objects, obj, varRegistry, "labelForObject"),
-            ]),
-            span (")", {class: "factText"}),
-            button ("", {
-                class: "addObjectToFact",
-                onclick: () => fact.objects.push(SearchState([state.objectRegistry, varRegistry])),
-            }),
-        ]),
-    ])
 
 const conceptList = (registry: Registry.T, list: () => Registry.SearchResult[]): HTMLElement =>
     div({}, [
@@ -329,6 +152,20 @@ document.body.prepend(
     div ({class: "separator"}),
 )
 
+type ParseResult = {result: "failure", reason: string} | {result: "success"}
+
+function parseRule(s: string): ParseResult {
+    if (s === "") {
+        return {result: "failure", reason: "Nothing entered."}
+    }
+    else {
+        return {result: "success"}
+    }
+}
+
+// Hack to prevent textareas from auto-inserting a tab char if the user is trying to delete it
+let deletingText = false
+
 app ("app", state,
     div ({class: "view"}, [
         div ({class: "databaseView"}),
@@ -340,35 +177,44 @@ app ("app", state,
                 }),
                 $for (() => state.rules, rule => [
                     div ({class: "rule"}, [
-                        div ({class: "ruleLabelBox"}, [
-                            textBox (rule.labelBoxState, {
-                                borderAlwaysVisible: false,
-                                inputTextStyle: "ruleLabelText",
-                                invalidInputTextStyle: "ruleLabelTextForNothing",
-                                onSubmit() {
-                                    if (!rule.labelBoxState.textIsValid ||
-                                        Registry.setConceptLabel(rule.ruleConcept, rule.labelBoxState.text) !== "success") {
-                                            rule.labelBoxState.text = rule.ruleConcept.label
-                                    }
-                                },
+                        div ({class: "ruleLabelBar"}, [
+                            input ({
+                                class: "ruleLabelText",
+                                value: toRefs(rule).label,
                             }),
                             button ("•••", {
                                 class: "ruleDragHandle",
                                 onclick: () => state.rules.removeAt(rule.$index),
                             }),
                         ]),
-                        div ({class: "ruleContent"}, [
-                            factEl (rule.head, rule.varRegistry),
-                            div ({class: "ruleBody"}, [
-                                $for (() => rule.body, fact => [
-                                    factEl (fact, rule.varRegistry),
-                                ]),
-                                button ("", { 
-                                    class: "premiseInsertionPoint",
-                                    onclick: () => newPremise(rule),
-                                }),
-                            ]),
-                        ]),
+                        textarea ({
+                            class: "ruleTextArea",
+                            value: toRefs(rule).rawText,
+                            onkeydown: (event: KeyboardEvent) => {
+                                deletingText = (event.key === "Backspace" || event.key === "Delete")
+                            },
+                            oninput: (event: Event) => {
+                                const el = (event.target as HTMLTextAreaElement)
+                                if (!deletingText) {
+                                    // Auto-insert a tab character if necessary
+                                    const fixedText = rule.rawText.replace(/\n([^\t]|$)/g, "\n\t$1")
+                                    if (fixedText !== el.value) {
+                                        toRefs(rule).rawText.value = fixedText
+                                        const start = el.selectionStart
+                                        const end = el.selectionEnd
+                                        setTimeout(() => {
+                                            // Move cursor position past inserted Tab
+                                            el.selectionStart = start+1
+                                            el.selectionEnd = end+1
+                                        }, 0)
+                                    }
+                                }
+                                const parseResult = parseRule(rule.rawText)
+                                // resize the box to fit its contents
+                                el.style.height = "auto"
+                                el.style.height = el.scrollHeight + "px"
+                            },
+                        }),
                     ]),
                     button ("", {
                         class: "ruleInsertionPoint",
@@ -377,10 +223,6 @@ app ("app", state,
                 ]),
             ]),
             div ({class: "separator"}),
-            p ("Create a relation:"),
-            conceptList (state.relationRegistry, () => state.allRelations),
-            p ("Create an object:"),
-            conceptList (state.objectRegistry, () => state.allObjects),
             div ({class: "viewBottomPadding"}),
         ]),
     ])
@@ -389,6 +231,12 @@ app ("app", state,
 // Disable right-click menu
 window.oncontextmenu = (e: Event): void => {
     e.preventDefault()
+}
+
+// Hack to make textareas stretch to fit content
+const textAreas = document.getElementsByTagName("textarea")
+for (let i = 0; i < textAreas.length; i++) {
+    textAreas[i].style.height = textAreas[i].scrollHeight + "px"
 }
 
 //#endregion
